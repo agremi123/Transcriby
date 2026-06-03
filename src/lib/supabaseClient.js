343e1add88bfb70550b28e331f17f0edd10ee442
agremi123 <b00459997@essec.edu>
2026-06-03 20:47:56 +0700

    Auto-commit: supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://sgnvxlnmbvhgscyuents.supabase.co';
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_JX4bB9aFm5LtPGGqfoCZaQ_tiDjNWY0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
