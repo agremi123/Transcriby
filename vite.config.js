1df99f2475946071eda001e4c502fee40f918b1b
agremi123 <b00459997@essec.edu>
2026-06-05 01:33:09 +0700

    Auto-commit: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -974,6 +974,8 @@ export default defineConfig(() => {
           server.middlewares.use(practiceMiddleware(env.ANTHROPIC_API_KEY));
           server.middlewares.use(readingMiddleware(env.ANTHROPIC_API_KEY));
           server.middlewares.use(listeningMiddleware(env.ANTHROPIC_API_KEY, env.DEEPGRAM_API_KEY));
+          server.middlewares.use(speakingPromptMiddleware(env.ANTHROPIC_API_KEY));
+          server.middlewares.use(writingPromptMiddleware(env.ANTHROPIC_API_KEY));
           server.middlewares.use(wordMiddleware(env.ANTHROPIC_API_KEY, env.ELEVENLABS_API_KEY, env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY));
           server.middlewares.use(elevenLabsTtsMiddleware(env.ELEVENLABS_API_KEY));
         },
@@ -986,6 +988,8 @@ export default defineConfig(() => {
           server.middlewares.use(practiceMiddleware(env.ANTHROPIC_API_KEY));
           server.middlewares.use(readingMiddleware(env.ANTHROPIC_API_KEY));
           server.middlewares.use(listeningMiddleware(env.ANTHROPIC_API_KEY, env.DEEPGRAM_API_KEY));
+          server.middlewares.use(speakingPromptMiddleware(env.ANTHROPIC_API_KEY));
+          server.middlewares.use(writingPromptMiddleware(env.ANTHROPIC_API_KEY));
           server.middlewares.use(wordMiddleware(env.ANTHROPIC_API_KEY, env.ELEVENLABS_API_KEY, env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY));
           server.middlewares.use(elevenLabsTtsMiddleware(env.ELEVENLABS_API_KEY));
         },
