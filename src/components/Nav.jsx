5a81b4b2877e566ab7475a284cf64702ba2bd1e2
agremi123 <b00459997@essec.edu>
2026-06-06 16:33:27 +0700

    Auto-commit: Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,10 +21,7 @@ function NavReachNextLevel() {
   );
 
   return (
-    <div className="relative hidden sm:inline-flex items-center gap-3 mt-6">
-      <div style={{ marginTop: 78, marginLeft: 62 }}>
-        <ParisianProfileSquare compact className="pointer-events-none" />
-      </div>
+    <div className="relative hidden sm:inline-flex items-center mt-6">
       <ParisianExperienceHint />
       <div className="relative">
         <Link
