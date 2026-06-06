169eed07edb243d7eff468f36ea7a7da2a695b81
agremi123 <b00459997@essec.edu>
2026-06-06 16:29:42 +0700

    Auto-commit: atoms.jsx

--- src/components/atoms.jsx
+++ src/components/atoms.jsx
@@ -5,7 +5,7 @@ import { ParisianProfileSquare } from './ParisianCornerBadge';
 
 function ParisianBadgeInline() {
   return (
-    <div style={{ marginTop: 78 }}>
+    <div style={{ marginTop: 78, marginLeft: 12 }}>
       <ParisianProfileSquare compact className="pointer-events-none" />
     </div>
   );
