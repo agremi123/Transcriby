1c3e96247ab7174778ea1a8fb13e3958b72136ec
agremi123 <b00459997@essec.edu>
2026-06-06 16:54:07 +0700

    Auto-commit: MyTargets.jsx

--- src/pages/MyTargets.jsx
+++ src/pages/MyTargets.jsx
@@ -166,6 +166,7 @@ function ArcPathMap({ grouped, progressMap, currentLevel, nextLevel }) {
   const PAD     = 22;
   const PAST_PX = 155;
   const FUT_PX  = 155;
+  const BADGE_SIZE = 210;
   const VX      = -(BADGE_SIZE / 2 - PAD + 8); // viewBox x-start so left badge isn't clipped
   const pastW   = pastLevels.length * PAST_PX;
   const futW    = futureLevels.length * FUT_PX;
