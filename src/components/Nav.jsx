82d3a25b15ae72dafaabed26dc6f092f3e7bfa2e
agremi123 <b00459997@essec.edu>
2026-06-13 21:49:13 +0700

    Auto-commit: Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,7 +25,7 @@ function NavReachNextLevel() {
       <div className="absolute right-full pointer-events-none" style={{ marginTop: 65, marginRight: -20 }}>
         <ParisianProfileSquare compact className="pointer-events-none" />
       </div>
-      <ParisianExperienceHint placement="bottom" />
+      <ParisianExperienceHint />
       <div className="relative">
         <Link
           to="/targets"
