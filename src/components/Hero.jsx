ae2f702861d9d8349a157eac382fb44554fecdc0
agremi123 <b00459997@essec.edu>
2026-06-14 01:44:02 +0700

    Auto-commit: Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6177,7 +6177,7 @@ function WritingChallengePanel({ loading, prompt = '', tips = {}, wordTarget = 8
 }
 
 export default function Hero() {
-  const { effectiveLevel, profile, spendExperience, dailyParisianPoints } = useLearnerProfile();
+  const { effectiveLevel, profile, spendExperience, gainExperience, dailyParisianPoints } = useLearnerProfile();
   const [searchParams, setSearchParams] = useSearchParams();
   const [heroActiveTab, setHeroActiveTab] = React.useState('transcript');
   const learnMode = searchParams.get('learn');
