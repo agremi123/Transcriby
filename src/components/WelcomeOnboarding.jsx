5de2c490b8ac75abd703a12e55d069dd25269b4d
agremi123 <b00459997@essec.edu>
2026-06-19 00:20:44 +0700

    Auto-commit: WelcomeOnboarding.jsx

--- src/components/WelcomeOnboarding.jsx
+++ src/components/WelcomeOnboarding.jsx
@@ -560,7 +560,7 @@ export default function WelcomeOnboarding() {
                           >
                             <motion.div
                               layoutId={SELECTED_BADGE_LAYOUT}
-                              className="flex items-center justify-center w-[200px] h-[200px] sm:w-[230px] sm:h-[230px] rounded-full border border-wine ring-2 ring-wine/25 ring-offset-2 ring-offset-paper bg-paper p-2.5 shadow-md"
+                              className="flex items-center justify-center w-[150px] h-[150px] sm:w-[160px] sm:h-[160px] rounded-full border border-wine ring-2 ring-wine/25 ring-offset-2 ring-offset-paper bg-paper p-2.5 shadow-md"
                             >
                               <img
                                 src={getLevelBadgeSrc(pickedLevel)}
